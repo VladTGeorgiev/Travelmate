ae1af0c04d6bb5c320552454b4ab1481aca88a51
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-07-12 10:49:32 +0100

    fixed create user bug

--- backend/app/services/landmark_serializer.rb
+++ backend/app/services/landmark_serializer.rb
@@ -6,7 +6,7 @@ class LandmarkSerializer
 
     def to_serialized_json
         @landmark.to_json(:include => {
-        :comments => {:only => [:id, :description, :user_id]},
+        :comments => {:only => [:id, :description, :user_id, :username]},
       }, :except => [:created_at, :updated_at])
     end
     
